d0b21a7916bd097f14274f22fc6e1fc8817a1582
Justine <136057314+Delurs@users.noreply.github.com>
2024-02-06 08:11:11 +0800

    Create Greater of Two.py

---
+++ python1/if statement/Greater of Two.py
@@ -0,0 +1,8 @@
+a = int(input("Enter value a: "))
+b = int(input("Enter value b: "))
+if a > b:
+    print(f"{a} is greater than {b}.")
+if a < b:
+    print(f"{b} is greater than {a}.")
+if a == b:
+    print(f"{a} is equal to {b}.")
